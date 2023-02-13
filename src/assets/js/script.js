67db2843b365f100aec68fc8ac0024a4f3129051
RathGate <mariannecorbel@hotmail.fr>
2023-02-13 17:06:34 +0100

    Finished landing, menu and burger, not responsive

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,24 @@
 if (document.querySelector("aside") && document.querySelector(".menu")) {
     document.querySelector(".menu").addEventListener("click", function(){
-        document.querySelector(".green-screen").classList.add("green-screen-visible")
-        setTimeout(() => {  document.querySelector("aside").classList.toggle("menu-visible") }, 500)
+        if (document.querySelector("aside").classList.contains("menu-visible")) {
+            document.querySelector("aside").classList.toggle("menu-visible")
+            document.querySelector(".green-screen").remove()
+            let div = document.createElement("div"); div.classList.add("green-screen")
+            document.querySelector("header").appendChild(div)
+        } else {
+            document.querySelector(".green-screen").classList.add("green-screen-visible")
+            setTimeout(() => { document.querySelector("aside").classList.toggle("menu-visible")}, 500)
+        }
     })
 }
+
+document.querySelectorAll(".sub").forEach(element => {
+    if (element.querySelector(".subtitle-container")) {
+        element.addEventListener("mouseover", function() {
+            element.querySelector(".subtitle-container").classList.add("visible")
+        })
+        element.addEventListener("mouseout", function() {
+            element.querySelector(".subtitle-container").classList.remove("visible")
+        })
+    }
+})
